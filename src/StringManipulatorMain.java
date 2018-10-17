f0749cf802a32a37a99915a3b52b65d3f9fa045b
jh1923 <42978576+jh1923@users.noreply.github.com>
2018-10-17 14:28:09 -0400

    Update StringManipulatorMain.java

--- src/StringManipulatorMain.java
+++ src/StringManipulatorMain.java
@@ -5,10 +5,39 @@ public class StringManipulatorMain
 
 	public static void main(String[] args)
 	{
-		Scanner in = new Scanner(System.in);
-		System.out.print("Enter a string: ");
-		String input = in.next();
-		System.out.println(StringManipulator.noVowels(input));
+		boolean tryAgain = true;
+		while (tryAgain)
+		{
+			Scanner in = new Scanner(System.in);
+			System.out.println("Would you like to run a method? (y/n): ");
+			String tryDecision = (in.next()).toLowerCase();
+			in.nextLine();
+			if (tryDecision.charAt(0) == 'y')
+			{
+				System.out.println("Which method would you like to run?\n1. noVowels \n2. reverse \nEnter '1' or '2': ");
+				int choice = in.nextInt();
+				in.nextLine();
+				
+				if (choice == 1)
+				{
+					System.out.println("Enter a string: ");
+					String str = in.next();
+					in.nextLine();
+					System.out.println(StringManipulator.noVowels(str));
+				}
+				
+				else if (choice == 2)
+				{
+					System.out.println("Enter a string: ");
+					String str = in.next();
+					in.nextLine();
+					System.out.println(StringManipulator.reverse(str));
+				}
+			}
+			else
+			{
+				tryAgain = false;
+			}
+		}
 	}
-
 }
